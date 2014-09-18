85a59acf2ee4fca4d85487d36f5c148b98f101a8
zs <baloghzsof@gmail.com>
2014-09-18 11:52:34 +0200

    Approve/unapprove pr based on the build status.

--- codeship_build.rb
+++ codeship_build.rb
@@ -12,12 +12,13 @@
 #  commit_id          :string
 #  short_commit_id    :string
 #  message            :text
-#  source_commit_hash :string
 #  committer          :string
 #  branch             :string
 #  created_at         :datetime
 #  updated_at         :datetime
 #
+
+require './bitbucket_pull_request'
 
 class CodeshipBuild < Sequel::Model
 
@@ -27,6 +28,14 @@ class CodeshipBuild < Sequel::Model
 
   STATUSES = ['testing', 'error', 'success', 'stopped', 'waiting']
 
+  def bitbucket_pull_request
+    BitbucketPullRequest.first source_commit_hash: bitbucket_commit_hash
+  end
+
+  def bitbucket_commit_hash
+    commit_id[0...12]
+  end
+
   def validate
     super
     validates_presence [:build_url, :commit_url, :project_id,
@@ -34,6 +43,18 @@ class CodeshipBuild < Sequel::Model
                         :short_commit_id, :message, :committer, :branch]
     validates_unique :build_id
     validates_includes self.class::STATUSES, :status
+  end
+
+  def after_save
+    super
+    return if bitbucket_pull_request.nil?
+
+    case status
+    when 'success'
+      bitbucket_pull_request.approve!
+    else
+      bitbucket_pull_request.unapprove!
+    end
   end
 
 end
